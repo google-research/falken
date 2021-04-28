2b25d40fc757dcce0c36632bf0a27eebc9c60a49
Stewart Miles <smiles@google.com>
2021-04-28 16:45:35 -0700

    Only install modules that are not already installed.
    PiperOrigin-RevId: 371012871
    Change-Id: I7f0550a966d4ae83e8588c5b5116b47208abd0a3

--- service/common/pip_installer.py
+++ service/common/pip_installer.py
@@ -14,25 +14,81 @@
 
 # Lint as: python3
 """Installs dependencies using pip."""
+
+import logging
 import os
 import subprocess
 import sys
 
 # Modules required to execute service modules and tests.
 _REQUIRED_PYTHON_MODULES = [
-    'absl-py',
-    'tensorflow',
-    'tensorflow-graphics',
-    'tf-agents==0.8.0rc1',
-    'grpcio-tools',
-    'flatbuffers'
+    ('absl-py', ''),
+    ('tensorflow', ''),
+    ('tensorflow-graphics', ''),
+    ('tf-agents', '==0.8.0rc1'),
+    ('grpcio-tools', ''),
+    ('flatbuffers', ''),
 ]
+
+_PIP_INSTALL_ARGS = [sys.executable, '-m', 'pip', 'install']
+_PIP_SHOW_ARGS = [sys.executable, '-m', 'pip', '-q', 'show']
+# Cache of installed modules populated by _module_installed().
+_INSTALLED_MODULE_LIST = []
+
+
+def _clear_installed_modules_cache():
+  """Flush cache of installed modules."""
+  global _INSTALLED_MODULE_LIST
+  _INSTALLED_MODULE_LIST = []
+
+
+def _module_installed(module):
+  """Determine whether a module is installed.
+
+  Args:
+    module: Name of the Python module to query.
+
+  Returns:
+    True if installed, False otherwise.
+
+  Raises:
+    subprocess.CalledProcessError: If pip fails to list modules.
+  """
+  global _INSTALLED_MODULE_LIST
+  if not _INSTALLED_MODULE_LIST:
+    result = subprocess.run([sys.executable, '-m', 'pip', 'list'],
+                            stdout=subprocess.PIPE, check=True)
+    # Each line consists of "module_name\w+version", extract the module name
+    # from each line.
+    _INSTALLED_MODULE_LIST = [
+        l.split()[0] for l in result.stdout.decode('utf-8').splitlines()]
+  return module in _INSTALLED_MODULE_LIST
+
+
+def _install_module(module, version):
+  """Install a Python module.
+
+  Args:
+    module: Name of the module to install.
+    version: Version constraints for the module to install or an empty string
+      to install the latest.
+
+  Raises:
+    subprocess.CalledProcessError: If module installation fails.
+  """
+  logging.info('Installing Python module %s...', module)
+  subprocess.check_call(_PIP_INSTALL_ARGS + [f'{module}{version}'])
 
 
 def install_dependencies():
   """Install all Python module dependencies."""
-  for m in _REQUIRED_PYTHON_MODULES:
-    subprocess.check_call([sys.executable, '-m', 'pip', '-q', 'install', m])
+  modules_installed = False
+  for module, version in _REQUIRED_PYTHON_MODULES:
+    if not _module_installed(module):
+      _install_module(module, version)
+      modules_installed = True
+  if modules_installed:
+    _clear_installed_modules_cache()
 
 
 if int(os.environ.get('FALKEN_AUTO_INSTALL_DEPENDENCIES', 1)):
--- service/common/pip_installer_test.py
+++ service/common/pip_installer_test.py
@@ -32,15 +32,89 @@ import pip_installer
 class PipInstallerTest(absltest.TestCase):
   """Test generate_protos module."""
 
-  @mock.patch.object(subprocess, 'check_call', autospec=True)
-  def test_install_dependencies(self, check_call):
-    """Import the install_dependencies module and verify install."""
+  # Module to install in the local Python site-library to test installation.
+  _TEST_MODULE = 'pip-install-test'
+
+  def tearDown(self):
+    """Reset global state between tests."""
+    super().tearDown()
+    pip_installer._clear_installed_modules_cache()
+
+  @classmethod
+  def _uninstall_test_module(cls):
+    """Uninstall the test Python module."""
+    subprocess.run([sys.executable, '-m', 'pip', 'uninstall', '-y',
+                    cls._TEST_MODULE], check=False)
+
+  def test_module_installed(self):
+    """Test checking for an installed module."""
+    try:
+      self._uninstall_test_module()
+      self.assertFalse(pip_installer._module_installed(self._TEST_MODULE))
+      subprocess.check_call([sys.executable, '-m', 'pip', 'install',
+                             self._TEST_MODULE])
+      pip_installer._clear_installed_modules_cache()
+      self.assertTrue(pip_installer._module_installed(self._TEST_MODULE))
+    finally:
+      self._uninstall_test_module()
+
+  def test_install_module(self):
+    """Test module installation."""
+    try:
+      self._uninstall_test_module()
+      pip_installer._install_module(self._TEST_MODULE, '')
+      subprocess.check_call([sys.executable, '-m', 'pip', 'show',
+                             self._TEST_MODULE])
+    finally:
+      self._uninstall_test_module()
+
+  @mock.patch.object(pip_installer, '_clear_installed_modules_cache')
+  @mock.patch.object(pip_installer, '_module_installed')
+  @mock.patch.object(pip_installer, '_install_module')
+  def test_install_dependencies(self, mock_install_module,
+                                mock_module_installed,
+                                mock_clear_installed_modules_cache):
+    """Install all module dependencies."""
+    mock_module_installed.return_value = False
     pip_installer.install_dependencies()
-    expected_call_args_list = [
-        mock.call([sys.executable, '-m', 'pip', '-q', 'install', module])
-        for module in pip_installer._REQUIRED_PYTHON_MODULES
-    ]
-    check_call.assert_has_calls(expected_call_args_list)
+
+    mock_module_installed.assert_has_calls([
+        mock.call(m) for m, _ in pip_installer._REQUIRED_PYTHON_MODULES
+    ])
+    mock_install_module.assert_has_calls([
+        mock.call(m, v) for m, v in pip_installer._REQUIRED_PYTHON_MODULES
+    ])
+    mock_clear_installed_modules_cache.assert_called()
+
+  @mock.patch.object(pip_installer, '_clear_installed_modules_cache')
+  @mock.patch.object(pip_installer, '_module_installed')
+  @mock.patch.object(pip_installer, '_install_module')
+  @mock.patch.object(pip_installer, '_REQUIRED_PYTHON_MODULES',
+                     [('foo', ''), ('bar', '')])
+  def test_install_missing_dependencies(self, mock_install_module,
+                                        mock_module_installed,
+                                        mock_clear_installed_modules_cache):
+    """Only install modules that are missing."""
+    mock_module_installed.side_effect = lambda module: module == 'bar'
+
+    pip_installer.install_dependencies()
+    mock_install_module.assert_called_with('foo', '')
+    mock_clear_installed_modules_cache.assert_called()
+
+  @mock.patch.object(pip_installer, '_clear_installed_modules_cache')
+  @mock.patch.object(pip_installer, '_module_installed')
+  @mock.patch.object(pip_installer, '_install_module')
+  @mock.patch.object(pip_installer, '_REQUIRED_PYTHON_MODULES',
+                     [('foo', ''), ('bar', '')])
+  def test_install_no_dependencies(self, mock_install_module,
+                                   mock_module_installed,
+                                   mock_clear_installed_modules_cache):
+    """Install no modules if they're all installed."""
+    mock_module_installed.return_value = True
+
+    pip_installer.install_dependencies()
+    mock_install_module.assert_not_called()
+    mock_clear_installed_modules_cache.assert_not_called()
 
 
 if __name__ == '__main__':

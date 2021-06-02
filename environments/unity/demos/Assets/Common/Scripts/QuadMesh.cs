e2c71d96f332146f4131ab268c2288d6c49da4ca
Nathan Martz <nathanmartz@google.com>
2021-06-02 11:22:11 -0700

    Allow cityscapes to have randomly generated building colors.
    Change-Id: I2ea872e3bae556f7d7283cf27576f4e2cb09450b

--- environments/unity/demos/Assets/Common/Scripts/QuadMesh.cs
+++ environments/unity/demos/Assets/Common/Scripts/QuadMesh.cs
@@ -24,6 +24,7 @@ class QuadMesh {
         public Vector3[] positions;
         public Vector3[] normals;
         public Vector2[] uvs;
+        public Color[] colors;
     }
 
     private List<Quad> quads = new List<Quad>();
@@ -31,17 +32,31 @@ class QuadMesh {
     /// <summary>
     /// Adds a single quad to the mesh datastructure.
     /// </summary>
-    public void AddQuad(Vector3[] positions, Vector3[] normals, Vector2[] uvs,
+    public void AddQuad(Vector3[] positions, Vector3[] normals, Vector2[] uvs, Color[] colors,
         Transform transform) {
         Debug.Assert(positions.Length == 4);
         Debug.Assert(normals.Length == 4);
         Debug.Assert(uvs.Length == 4);
-        Quad quad = new Quad { positions = positions, normals = normals, uvs = uvs };
+        Debug.Assert(colors == null || colors.Length == 4);
+        Quad quad = new Quad {
+            positions = positions, normals = normals, uvs = uvs, colors = colors
+        };
+        if (quad.colors == null) {
+            quad.colors = new Color[] { Color.white, Color.white, Color.white, Color.white };
+        }
         for (int i = 0; i < 4; ++i) {
             quad.positions[i] = transform.InverseTransformPoint(quad.positions[i]);
             quad.normals[i] = transform.InverseTransformDirection(quad.normals[i]);
         }
         quads.Add(quad);
+    }
+
+    /// <summary>
+    /// Adds a single quad to the mesh datastructure.
+    /// </summary>
+    public void AddQuad(Vector3[] positions, Vector3[] normals, Vector2[] uvs,
+        Transform transform) {
+        AddQuad(positions, normals, uvs, null, transform);
     }
 
     /// <summary>
@@ -52,12 +67,14 @@ class QuadMesh {
         List<Vector3> positions = new List<Vector3>(mesh.vertices);
         List<Vector3> normals  = new List<Vector3>(mesh.normals);
         List<Vector2> uvs = new List<Vector2>(mesh.uv);
+        List<Color> colors = new List<Color>(mesh.colors);
         List<int> indices = subMesh ? new List<int>() :  new List<int>(mesh.triangles);
         for (int i = 0; i < quads.Count; ++i) {
             int baseIndex = positions.Count;
             positions.AddRange(quads[i].positions);
             normals.AddRange(quads[i].normals);
             uvs.AddRange(quads[i].uvs);
+            colors.AddRange(quads[i].colors);
             int[] quadIndices = { baseIndex + 0, baseIndex + 1, baseIndex + 2,
                                   baseIndex + 0, baseIndex + 2, baseIndex + 3 };
             indices.AddRange(quadIndices);
@@ -65,6 +82,7 @@ class QuadMesh {
         mesh.vertices = positions.ToArray();
         mesh.normals = normals.ToArray();
         mesh.uv = uvs.ToArray();
+        mesh.colors = colors.ToArray();
 
         if (subMeshIndex >= 0) {
             Debug.Assert(subMeshIndex < mesh.subMeshCount);
